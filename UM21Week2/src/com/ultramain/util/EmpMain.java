6b2d1d9819300ca4b644bbc260490d9dd26cbdb7
ANatarajan <ANatarajan@USI-4202L.ultramain.com>
2021-09-04 09:48:31 +0530

    DataConnect

---
+++ UM21Week2/src/com/ultramain/util/EmpMain.java
@@ -0,0 +1,10 @@
+package com.ultramain.util;
+
+import com.ultramain.business.EmployeeBO;
+
+public class EmpMain {
+	public static void main(String[] args) {
+		EmployeeBO bo = new EmployeeBO();
+		bo.empSystem();
+	}
+}
